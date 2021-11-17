8c4f0a61886fe50fb6e65407e6df3e3398c6841d
Saqib Attiq <saqibattiq7@gmail.com>
2021-11-17 19:48:09 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="container">
+      <div className="item1">
+      <div className="menu" ><img src="https://img.icons8.com/ios/50/000000/menu--v1.png"/>
+      </div>
+          <div className="logo"><h6>USER PLACES</h6></div>
+          <nav>
+              <a href="#all users">ALL USERS</a>
+              <a href="#sign in">SIGN IN</a>
+              <a href="#sign up">SIGN UP</a>
+            </nav>
+      </div>
+      <div className="item2">
+        <div className="innercontainer">
+          <div className="inneritem1">
+            <div className="img">
+              <img src="th.jpg" alt="th.jpg" />
+            </div>
+            <div className="heading">
+                <p>Mirza Babar Muneer Baig</p>
+                <p>3 Places</p>
+              </div>
+          </div>
+          <div className="inneritem1">
+            <div className="img">
+              <img src="af7a6480430127ba8df9e168d71e44f9.jpg" alt="th.jpg" />
+            </div>
+            <div className="heading">
+                <p>Mirza Babar Muneer</p>
+                <p>3 Places</p>
+              </div>
+          </div>
+          <div className="inneritem1">
+            <div className="img">
+              <img src="791762.jpg" alt="th.jpg" />
+            </div>
+            <div className="heading">
+                <p>Mirza Babar 🐱</p>
+                <p>3 Places</p>
+              </div>
+          </div>
+          <div className="inneritem1">
+            <div className="img">
+              <img src="OIP.jpg" alt="th.jpg" />
+            </div>
+            <div className="heading">
+                <p>Bahu Balie 🤣🎉
+                </p>
+                <p>3 Places</p>
+              </div>
+          </div>
+          <div className="inneritem1">
+            <div className="img">
+              <img src="wallpapertip_hacker-wallpaper_21361.jpg" alt="th.jpg" />
+            </div>
+            <div className="heading">
+                <p>Ali Muneer Baig</p>
+                <p>3 Places</p>
+              </div>
+          </div>
+          <div className="inneritem1">
+            <div className="img">
+              <img src="OIP.jpg" alt="th.jpg" />
+            </div>
+            <div className="heading">
+                <p>Mirza Ali Muneer👀</p>
+                <p>3 Places</p>
+              </div>
+          </div>
+          <div className="inneritem1 center1">
+            <div className="img">
+              <img src="th.jpg" alt="th.jpg" />
+            </div>
+            <div className="heading">
+                <p>muneer Baig</p>
+                <p>3 Places</p>
+              </div>
+          </div>
+          <div className="inneritem1">
+            <div className="img">
+              <img src="menu.png" alt="th.jpg" />
+            </div>
+            <div className="heading">
+                <p>Mirza Babar Baig</p>
+                <p>3 Places</p>
+              </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
